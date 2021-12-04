c5e29de59018acd72201ecea60b277600b14f092
xt0r3 <kgs37@cam.ac.uk>
2021-12-04 13:51:48 +0100

    6th commit - dropdown fix attempt on mobiles

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,13 +1,31 @@
 function toggle(id, btn){
     let obj = document.getElementById(id);
-    btn.classList.toggle('open');
-    if(obj.style.maxHeight=='100%'){
-        obj.style.maxHeight = '0';
+    if(obj.style.maxHeight===obj.scrollHeight.toString()+"px"){
         btn.textContent = 'Bővebb információ';
+        obj.style.maxHeight = '0';
     }
     else{
-        obj.style.maxHeight = '100%';
         btn.textContent = 'Kevesebb információ';
+        obj.style.maxHeight = obj.scrollHeight.toString()+"px";
+        console.log(obj.scrollHeight.toString());
+    }
 
+    btn.classList.toggle('open');
+    obj.classList.toggle('open');
+}
+
+const objects = document.getElementsByClassName('dropdown-text');
+
+function rescale(){
+    for(let i = 0; i < objects.length; i++){
+        if(objects[i].classList.contains('open')){
+            objects[i].style.maxHeight = objects[i].scrollHeight.toString()+"px";
+        }
+        else{
+            objects[i].style.maxHeight = '0';
+
+        }
     }
 }
+
+window.addEventListener('resize', rescale);
